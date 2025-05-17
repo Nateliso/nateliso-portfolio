90e00fbb52f0508af52bc23056091e993870bc30
Liso Mhlana <mhlanaliso18@gmail.com>
2025-05-17 09:25:17 +0200

    Complete portfolio, ready for deployement

--- src/data/projectImages.js
+++ src/data/projectImages.js
@@ -2,7 +2,7 @@ import weatherApp from "../assets/weather-app.png";
 import expenseTracker from "../assets/expense-tracker.png";
 import triviaGame from "../assets/trivia-game.png";
 import movieExplora from "../assets/Movie-Explora.png";
-import todo from "../assets/todo.png";
+import todo from "../assets/to-do.png";
 import landingPage from "../assets/landing-page.png";
 import natesTechHaven from "../assets/Tech-Haven.png";
 
